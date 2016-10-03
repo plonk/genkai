57f45b1e6b4de1ca393fcf419c009018bb9d9131
Yoteichi <plonk@piano.email.ne.jp>
2016-10-04 03:36:51 +0900

    subject.txtの作成ロジックを分離。

---
+++ thread_list_renderer.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require_relative 'string_helpers'
+
+module Genkai
+  class ThreadListRenderer
+    include StringHelpers
+
+    def initialize(threads)
+      @threads = threads
+    end
+
+    def render
+      @threads.sort_by(&:mtime).reverse # いちばん最近更新されたものが先頭。
+           .map { |thread| "#{thread.id}.dat<>#{escape_field(thread.subject)} (#{thread.posts.size})\n" }
+           .join
+    end
+  end
+end
