eedcf02e9e36b4cd29e297f4a4d799aeb5f4b4d6
Yoteichi <plonk@piano.email.ne.jp>
2023-07-31 03:33:18 +0900

    ダークモード調整。

--- public/common.js
+++ public/common.js
@@ -1,9 +1,2 @@
 $(function () {
-  // クラスを追加、削除した方がいいな。
-  $('.media').on('mouseenter', function () {
-    $(this).css('background-color', '#f0f0f0');
-  });
-  $('.media').on('mouseleave', function () {
-    $(this).css('background-color', 'transparent');
-  });
 });
